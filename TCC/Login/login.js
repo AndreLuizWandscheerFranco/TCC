db8f4e4e7580de21f2eed735cb3b0717441e3f09
heroheroL <andreluizwandscheer@gmail.com>
2024-05-17 17:32:03 -0300

    Add files via upload

--- TCC/Login/login.js
+++ TCC/Login/login.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login_button');
 
 const eventofoco = ({target}) => {
    const span = target.previousElementSibling;
@@ -11,5 +12,17 @@ const eventofocoout = ({target}) => {
     span.classList.remove('span-ativo');
  }
 }
+
+const eventoinserir = () => {
+   const [username, password] = inputs;
+
+   if(username.value && password.value.length >= 8 ) {
+      button.removeAttribute('disabled')
+   }else{
+      button.setAttribute('disabled','')
+   }
+}
+
 inputs.forEach( (input) => input.addEventListener('focus', eventofoco) );
 inputs.forEach( (input) => input.addEventListener('focusout', eventofocoout) );
+inputs.forEach( (input) => input.addEventListener('input', eventoinserir) );
