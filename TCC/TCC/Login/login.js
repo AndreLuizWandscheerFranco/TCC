a50892734817df9b63e80d615cfc845818d8cb38
heroheroL <andreluizwandscheer@gmail.com>
2024-08-21 16:30:21 -0300

    Add files via upload

---
+++ TCC/TCC/Login/login.js
@@ -0,0 +1,35 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form');
+
+const eventofoco = ({target}) => {
+   const span = target.previousElementSibling;
+   span.classList.add('span-ativo');
+}
+
+const eventofocoout = ({target}) => {
+    if(target.value === ''){
+    const span = target.previousElementSibling;
+    span.classList.remove('span-ativo');
+ }
+}
+
+const login_button = (event) => {
+   window.location = "../Site/site.html";
+ };
+
+const eventoinserir = () => {
+   const [email, password] = inputs;
+
+   if(email.value && password.value.length >= 8 ) {
+      button.removeAttribute('disabled');
+      button.addEventListener('click',login_button);
+   }else{
+      button.setAttribute('disabled','');
+   }
+};
+
+inputs.forEach( (input) => input.addEventListener('focus', eventofoco) );
+inputs.forEach( (input) => input.addEventListener('focusout', eventofocoout) );
+inputs.forEach( (input) => input.addEventListener('input', eventoinserir) );
+form.addEventListener('submit', formsubmit);
