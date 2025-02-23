4a68c4adf351ae89cfb0c6308de773890f766793
André Luíz <andreluizwandscheer@gmail.com>
2025-02-23 18:13:18 -0300

    Add files via upload

--- TCC/carrinho/carrinho.js
+++ TCC/carrinho/carrinho.js
@@ -8,3 +8,15 @@ a.addEventListener("mouseenter", function () {
 a.addEventListener("mouseleave", function () {
     cart.classList.replace("bi-cart", "bi-cart-fill");
 });
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > lastScrollY) {
+        header.classList.add("hidden");
+    } else {
+        header.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+});
