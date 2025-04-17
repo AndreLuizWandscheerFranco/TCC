98c3bde5e8b110685ea8963c3f5f86fbe14eabbe
André Luíz <andreluizwandscheer@gmail.com>
2025-04-16 22:32:35 -0300

    Add files via upload

--- TCC/Site/site.js
+++ TCC/Site/site.js
@@ -1,4 +1,6 @@
 var cart = document.getElementById("cart");
+var arrowL = document.querySelector(".bi-caret-left");
+var arrowR = document.querySelector(".bi-caret-right");
 var a = document.getElementById("acart");
 
 const produtos = [
@@ -38,4 +40,55 @@ VanillaTilt.init(document.querySelectorAll(".produto"), {
     "max-glare": 0.1,
 });
 
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".nav.prev");
+const nextBtn = document.querySelector(".nav.next");
+let current = 0;
+
+function showSlide(index) {
+    slides[current].classList.remove("active");
+    current = (index + slides.length) % slides.length;
+    slides[current].classList.add("active");
+}
+
+nextBtn.addEventListener("click", () => {
+    showSlide(current + 1);
+});
+
+prevBtn.addEventListener("click", () => {
+    showSlide(current - 1);
+});
+
+setInterval(() => {
+    showSlide(current + 1);
+}, 10000);
+
+arrowL.addEventListener("mouseenter", function () {
+    arrowL.classList.replace("bi-caret-left", "bi-caret-left-fill");
+});
+
+arrowL.addEventListener("mouseleave", function () {
+    arrowL.classList.replace("bi-caret-left-fill", "bi-caret-left");
+});
+
+arrowR.addEventListener("mouseenter", function () {
+    arrowR.classList.replace("bi-caret-right", "bi-caret-right-fill");
+});
+
+arrowR.addEventListener("mouseleave", function () {
+    arrowR.classList.replace("bi-caret-right-fill", "bi-caret-right");
+});
+
+var msgCookies = document.getElementById("cookies-msg");
+
+function aceito() {
+    localStorage.lgpd = "sim";
+    msgCookies.classList.remove("mostrar");
+}
+
+if (localStorage.lgpd == "sim") {
+    msgCookies.classList.remove("mostrar");
+} else {
+    msgCookies.classList.add("mostrar");
+}
 
