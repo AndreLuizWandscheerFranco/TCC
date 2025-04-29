61a9d9d8b97fc73fa4fcef5c14d9417e8f37f57b
victor esemann lunelli <lunellivictor950@gmail.com>
2025-04-29 11:16:53 -0300

    Add files via upload

---
+++ TCC-Victor/TCC/cadastro/cadastro.js
@@ -0,0 +1,137 @@
+const inputs = document.querySelectorAll(".input");
+const button = document.querySelector(".login_button");
+const erro_input = document.querySelector(".erro_input");
+const erro_input_senha = document.querySelector(".erro_input_senha");
+const erro_input_nome = document.querySelector(".erro_input_nome");
+
+let timeoutId;
+
+const limpar_inputs = () => {
+    inputs.forEach((input) => {
+        input.value = "";
+        input.classList.remove("valido", "invalido");
+        erro_input.style.display = "none";
+        erro_input_senha.style.display = "none";
+    });
+    button.setAttribute("disabled", "");
+};
+
+window.addEventListener("pageshow", limpar_inputs);
+
+const digitado = {
+    nome: false,
+    email: false,
+    senha: false,
+};
+
+const eventofoco = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.add("span-ativo");
+};
+
+const eventofocoout = ({ target }) => {
+    if (target.value === "") {
+        const span = target.previousElementSibling;
+        span.classList.remove("span-ativo");
+    }
+
+    digitado[target.name] = true;
+    eventoinserir();
+};
+
+const login_button = (event) => {
+    event.preventDefault();
+    window.location = "../Site/site.html";
+};
+
+const eventoinserir = () => {
+    const [nome, email, senha] = inputs;
+
+    if (digitado.nome) {
+        if (nome.value.trim() !== "") {
+            nome.classList.add("valido");
+            nome.classList.remove("invalido");
+            erro_input_nome.style.display = "none";
+        } else {
+            nome.classList.add("invalido");
+            nome.classList.remove("valido");
+            erro_input_nome.style.display = "block";
+        }
+    }
+
+    if (validarEmail(email.value)) {
+        email.classList.add("valido");
+        email.classList.remove("invalido");
+        erro_input.style.display = "none";
+    } else if (email.value !== "") {
+        email.classList.add("invalido");
+        email.classList.remove("valido");
+        erro_input.style.display = "block";
+    } else {
+        erro_input.style.display = "none";
+        email.classList.remove("invalido");
+        email.classList.remove("valido");
+    }
+
+    if (senha.value.length >= 8 && validarSenha(senha.value)) {
+        senha.classList.add("valido");
+        senha.classList.remove("invalido");
+        erro_input_senha.style.display = "none";
+    } else if (senha.value !== "") {
+        senha.classList.add("invalido");
+        senha.classList.remove("valido");
+        erro_input_senha.style.display = "block";
+    } else {
+        erro_input_senha.style.display = "none";
+        senha.classList.remove("invalido");
+        senha.classList.remove("valido");
+    }
+
+    if (
+        nome.value &&
+        validarEmail(email.value) &&
+        senha.value.length >= 8 &&
+        validarSenha(senha.value)
+    ) {
+        button.removeAttribute("disabled");
+        button.addEventListener("click", login_button);
+    } else {
+        button.setAttribute("disabled", "");
+    }
+};
+
+const eventoInput = (e) => {
+    digitado[e.target.name] = true;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        eventoinserir();
+    }, 1000);
+};
+
+const validarEmail = (email) => {
+    const regex = /^[^\s@]+@(gmail\.com|hotmail\.com|outlook\.com)$/i;
+    return regex.test(email);
+};
+
+const validarSenha = (senha) => {
+    const regex = /^[a-zA-Z0-9]+$/;
+    return regex.test(senha);
+};
+
+function mostrarsenha() {
+    var inputPass = document.getElementById("pass");
+    var BtnShowPass = document.getElementById("btn-senha");
+
+    if (inputPass.type === "password") {
+        inputPass.setAttribute("type", "text");
+        BtnShowPass.classList.replace("bi-eye", "bi-eye-slash");
+    } else {
+        inputPass.setAttribute("type", "password");
+        BtnShowPass.classList.replace("bi-eye-slash", "bi-eye");
+    }
+}
+
+inputs.forEach((input) => input.addEventListener("input", eventoInput));
+inputs.forEach((input) => input.addEventListener("focus", eventofoco));
+inputs.forEach((input) => input.addEventListener("focusout", eventofocoout));
---
+++ TCC-Victor/TCC/carrinho/carrinho.js
@@ -0,0 +1,33 @@
+var cart = document.getElementById("cart");
+var a = document.getElementById("acart");
+
+a.addEventListener("mouseenter", function () {
+    cart.classList.replace("bi-cart-fill", "bi-cart");
+});
+
+a.addEventListener("mouseleave", function () {
+    cart.classList.replace("bi-cart", "bi-cart-fill");
+});
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    const subMenu = document.querySelector("#sub-menu");
+
+    if (window.scrollY > lastScrollY) {
+        header.classList.add("hidden");
+        subMenu.classList.add("hidden2");
+    } else {
+        header.classList.remove("hidden");
+        subMenu.classList.remove("hidden2");
+    }
+    lastScrollY = window.scrollY;
+});
+
+VanillaTilt.init(document.querySelectorAll(".produto"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5,
+});
