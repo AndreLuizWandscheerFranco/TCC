78ac46fa64c982c623eb2287a37358de6969fb6a
André Luíz <andreluizwandscheer@gmail.com>
2025-07-01 11:07:45 -0300

    Add files via upload

--- banco de dados.sql
+++ banco de dados.sql
@@ -4,18 +4,18 @@ use Banco_de_dados;
 
 create table usuario(
 id_usuario int AUTO_INCREMENT primary key,
-Nome varchar(104) not null,
-cpf varchar(11) not null,
-telefone int(13) not null,
-pais varchar(56) not null,
-cep varchar(9) not null,
-rua varchar(52) not null,
-numero varchar(5) not null,
-cidade varchar(37) not null,
-estado varchar(17) not null,
-email varchar(254) not null,
-senha varchar(100) not null,
-tipo varchar(13) not null
+Nome varchar(104),
+cpf varchar(11),
+telefone int(13),
+pais varchar(56),
+cep varchar(9),
+rua varchar(52),
+numero varchar(5),
+cidade varchar(37),
+estado varchar(17),
+email varchar(254),
+senha varchar(100),
+tipo varchar(13)
 );
 
 create table pagamento(
