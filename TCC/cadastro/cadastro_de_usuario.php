a1312bc84d0100b8e27f50a3df7844453019cb82
victor esemann lunelli <lunellivictor950@gmail.com>
2025-07-08 11:01:03 -0300

    Add files via upload

--- TCC/cadastro/cadastro_de_usuario.php
+++ TCC/cadastro/cadastro_de_usuario.php
@@ -1,5 +1,6 @@
 <?php
     
+    // Conexão
     $conn = new mysqli("localhost", "root", "root", "Banco_de_dados");
      if ($conn->connect_error) die("Erro: " . $conn->connect_error);
     
@@ -8,31 +9,21 @@
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     
+
+    
     echo("$nome");
     echo("$email");
     echo("$senha");
  
-    $verifica = $conn->prepare("SELECT id_usuarios FROM usuarios WHERE email = ?");
-    $verifica->bind_param("s", $email);
-    $verifica->execute();
-    $resultado = $verifica->get_result();
-
-    if ($resultado->num_rows > 0) {
-        echo "Este e-mail já está cadastrado.";
-        exit(); 
-    }
-
-    $inserir = $conn->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
-    $inserir->bind_param("sss", $nome, $email, $senha);
-
-    if ($inserir->execute()) {
-        header("Location: ../Site/index.html");
-        exit();
+    //Monta uma string com inserção 
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email' , '$senha')";
+    
+    
+    
+    //Executa no banco de dados
+    if ($conn->query($sql) === TRUE) {
+        echo "Usuário cadastrado com sucesso!<br><a href='listar.php'>Ver lista</a>";
     } else {
         echo "Erro: " . $conn->error;
     }
-
-    $verifica->close(); 
-    $inserir->close();
-    $conn->close();
 ?>
