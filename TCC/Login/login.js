6ba291d33003aef701bf632f8927dc532177039c
André Luíz <andreluizwandscheer@gmail.com>
2025-07-08 10:59:56 -0300

    Add files via upload

--- TCC/Login/login.js
+++ TCC/Login/login.js
@@ -35,10 +35,6 @@ const eventofocoout = ({ target }) => {
     eventoinserir();
 };
 
-const login_button = (event) => {
-    window.location = "../Site/index.html";
-};
-
 const eventoinserir = () => {
     const [email, password] = inputs;
 
